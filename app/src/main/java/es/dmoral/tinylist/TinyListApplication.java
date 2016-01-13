d2c7c574e03087f7e5551378d8648d15babba33d
GrenderG <grenderg@gmail.com>
2016-01-13 02:10:24 +0100

    First commit.

---
+++ app/src/main/java/es/dmoral/tinylist/TinyListApplication.java
@@ -0,0 +1,14 @@
+package es.dmoral.tinylist;
+
+import android.app.Application;
+
+/**
+ * Created by grender on 13/01/16.
+ */
+public class TinyListApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/es/dmoral/tinylist/fragments/SavedListsFragment.java
@@ -0,0 +1,39 @@
+package es.dmoral.tinylist.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import es.dmoral.tinylist.R;
+
+public class SavedListsFragment extends Fragment {
+
+    public SavedListsFragment() {
+        // Required empty public constructor
+    }
+
+    public static SavedListsFragment newInstance() {
+        return new SavedListsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_saved_lists, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
